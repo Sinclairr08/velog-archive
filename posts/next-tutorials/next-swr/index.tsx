f3c15698d9f357ccc04281464142f22d1e1feaf6
Junhyuk Lee <ljhjoon00@gmail.com>
2022-05-22 21:21:48 +0900

    modify: Add additional codes at next-swr post

--- posts/next-tutorials/next-swr/index.tsx
+++ posts/next-tutorials/next-swr/index.tsx
@@ -1,4 +1,6 @@
 import useSWR from "swr";
+import axios from "axios";
+import { useEffect } from "react";
 
 interface ShopsReturn {
   ok: boolean;
@@ -6,6 +8,14 @@ interface ShopsReturn {
 }
 
 export default function Test() {
+  /*
+  const [shopList, setShopList] = useState<string[]>([]);
+
+  useEffect(() => {
+    axios.get("/api/shops").then((res) => setShopList(res.data.shops));
+  }, []);
+   */
+
   const { data, error } = useSWR<ShopsReturn>("/api/shops");
 
   return <h1>{data.ok}</h1>;
